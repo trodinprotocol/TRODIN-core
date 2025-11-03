cbd2f0d6cee8b48e2a140e8ba48637fdfe31c713
TRODIN Protocol <trodin.protocol@gmail.com>
2025-11-03 15:50:32 -0500

    Update index.js

--- sdk/index.js
+++ sdk/index.js
@@ -1,1 +1,29 @@
+// sdk/index.js
+class TRODINPay {
+  constructor({ apiKey, apiUrl = "https://api.trodinpay.com" }) {
+    this.apiKey = apiKey;
+    this.apiUrl = apiUrl;
+  }
 
+  async mint({ amountUSD, playerAddress, webhookUrl }) {
+    const response = await fetch(`${this.apiUrl}/create-checkout-session`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": this.apiKey,
+      },
+      body: JSON.stringify({
+        amountUSD,
+        playerAddress,
+        webhookUrl,
+      }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error || "Mint failed");
+
+    return data;
+  }
+}
+
+export { TRODINPay };
